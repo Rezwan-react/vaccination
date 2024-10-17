ea00b8d9c69fcf9a30ff60c5c72bf966efe552c7
Rezwan-react <mdrazwanislam8@gmail.com>
2024-10-18 02:01:53 +0600

    first commit

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Banner.css'
+function Banner() {
+  return (
+   <section className='banner'>
+        <div className="contianer">
+            
+        </div>
+   </section>
+  )
+}
+
+export default Banner
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from '../components/home/Home'
+import Banner from '../components/banner/Banner'
+
+function HomePage() {
+  return (
+    <>
+        <Home/>
+        <Banner/>
+    </>
+  )
+}
+
+export default HomePage
